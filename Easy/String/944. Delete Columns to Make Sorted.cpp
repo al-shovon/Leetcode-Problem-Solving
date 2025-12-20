ee3ea9477974e5e51b7d8516a6d6c59cb3a632ca
al-shovon <shovondab1@gmail.com>
2025-12-20 11:54:58 +0600

    Create 944. Delete Columns to Make Sorted.cpp

---
+++ Easy/String/944. Delete Columns to Make Sorted.cpp
@@ -0,0 +1,21 @@
+//944. Delete Columns to Make Sorted
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+
+        int n = strs.size();
+        int s = strs[0].size();
+        int ans = 0;
+
+        for(int i = 0; i<s; i++){
+            for(int j = 1; j < n; j++){
+                if(strs[j][i] < strs[j-1][i]){
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
