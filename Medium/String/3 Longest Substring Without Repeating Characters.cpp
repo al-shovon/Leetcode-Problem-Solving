7abb4d49d68badb78fa71c2576c7c63bcd9a4f24
al-shovon <shovondab1@gmail.com>
2025-12-20 12:44:54 +0600

    Create 3 Longest Substring Without Repeating Characters.cpp

---
+++ Medium/String/3 Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,24 @@
+//3. Longest Substring Without Repeating Characters
+
+//Bruteforce approch
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+
+        int n = strs.size();
+        int s = strs[0].size();
+        int ans = 0;
+
+        for(int i = 0; i<s; i++){
+            for(int j = 1; j < n; j++){
+                if(strs[j][i] < strs[j-1][i]){
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//approch 2
