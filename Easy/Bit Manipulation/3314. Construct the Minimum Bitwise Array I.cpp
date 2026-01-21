7395fb9191ca09de7e823b4b02797280cbd9f153
al-shovon <shovondab1@gmail.com>
2026-01-22 02:12:53 +0600

    Update 3314. Construct the Minimum Bitwise Array I.cpp

--- Easy/Bit Manipulation/3314. Construct the Minimum Bitwise Array I.cpp
+++ Easy/Bit Manipulation/3314. Construct the Minimum Bitwise Array I.cpp
@@ -25,3 +25,32 @@ public:
         
     }
 };
+
+
+//approch 2 bit manipulation
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums){
+        int n = nums.size();
+
+        for(int i = 0; i<n; i++){
+            if(nums[i] == 2) nums[i] = -1;
+            else{
+                
+                for(int j = 0; j<32;j++){
+                    if((nums[i] & (1<<j)) > 0) continue;
+                    else{
+                        nums[i] = (nums[i] ^ (1<<(j-1)));
+                        break;
+
+                    }
+
+                }
+
+            }
+        
+        }
+        return nums;
+        
+    }
+};
