dfb95b39f4f15d999fab7a676bb331b7ede1a78f
al-shovon <shovondab1@gmail.com>
2026-02-02 19:00:47 +0600

    Create 2966. Divide Array Into Arrays With Max Difference.cpp

---
+++ Medium/Array/2966. Divide Array Into Arrays With Max Difference.cpp
@@ -0,0 +1,31 @@
+//2966. Divide Array Into Arrays With Max Difference
+
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int start = 0;
+
+        vector<vector<int>>ans(n/3,vector<int>(3));
+        vector<vector<int>>temp;
+        for(int i = 0; i< n/3;i++){
+            for(int j = 0; j <3; j++){
+                if(j == 0){
+                     ans[i][j] = nums[start];
+                     start++;
+                }
+                else{
+                    if( ((start+1) %3 == 0) && (nums[start] - nums[start-2] >k )) return temp;
+                    else ans[i][j] = nums[start];
+                    start++;
+                }
+                
+
+            }
+        }
+
+        return ans;
+        
+    }
+};
