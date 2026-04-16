d008504a86ba2a6fcabab83cdea55d8055467296
al-shovon <shovondab1@gmail.com>
2026-04-16 11:29:10 +0600

    Create 231. Power of Two.cpp

---
+++ Easy/Bit Manipulation/231. Power of Two.cpp
@@ -0,0 +1,47 @@
+//231. Power of Two
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0) return 0;
+        return (n & (n-1))== 0 ? 1 : 0;
+        
+    }
+};
+
+
+//app 2-> loop
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 1) return true;
+        if(n < 0) return false;
+
+        double r = n;
+
+        while (r > 0)
+        {
+            if (r == 2)
+            {
+                return true;
+            }
+
+            r = r/2;
+            
+            
+        }
+        return false;
+        
+    }
+};
+
+//app 3-> optimized
+class Solution {
+public:
+   bool isPowerOfTwo(int n) {
+
+       
+       return n > 0 && (n &(n-1)) == 0;
+        
+        
+    }
+};
