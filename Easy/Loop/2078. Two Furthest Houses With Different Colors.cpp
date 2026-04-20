8ac77a42f06ead4fa1dc7d764aeeefadfc3dd78c
al-shovon <shovondab1@gmail.com>
2026-04-20 10:38:10 +0600

    Add solution for maximum distance between different colored houses
    Implement a solution to find the maximum distance between two houses of different colors.

--- Easy/Loop/2078. Two Furthest Houses With Different Colors.cpp
+++ Easy/Loop/2078. Two Furthest Houses With Different Colors.cpp
@@ -1,1 +1,16 @@
 //2078. Two Furthest Houses With Different Colors
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n = colors.size();
+        int ans = 0;
+
+        for(int i = 0; i<n;i++){
+            for(int j = i+1;j<n;j++){
+                if(colors[i] != colors[j]) ans = max(ans,abs(i-j));
+            }
+        }
+        return ans;
+        
+    }
+};
