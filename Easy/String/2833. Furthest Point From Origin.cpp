5cd08da2b9ad27d9aac832cc9ff9d4c791fae9fa
al-shovon <shovondab1@gmail.com>
2026-04-24 23:02:06 +0600

    Add solution for furthest point from origin problem
    Implement a solution to calculate the furthest distance from the origin based on movement commands.

---
+++ Easy/String/2833. Furthest Point From Origin.cpp
@@ -0,0 +1,21 @@
+//2833. Furthest Point From Origin
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int n = moves.size();
+        int  l = 0;
+        int  r = 0;
+        int  dash = 0;
+
+        for(int i = 0;i<n;i++){
+            if(moves[i] == 'L') l++;
+            else if(moves[i] == 'R') r++;
+            else dash++;
+
+        }
+        return abs(l-r) + dash;
+
+
+        
+    }
+};
