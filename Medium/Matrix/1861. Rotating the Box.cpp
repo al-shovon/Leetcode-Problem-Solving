0745bc3e9327445dddc2238a37efd2ea96288f30
al-shovon <shovondab1@gmail.com>
2026-05-07 11:57:28 +0600

    Implement multiple methods to rotate the box

--- Medium/Matrix/1861. Rotating the Box.cpp
+++ Medium/Matrix/1861. Rotating the Box.cpp
@@ -1,3 +1,4 @@
+//app 1
 //1861. Rotating the Box
 class Solution {
 public:
@@ -38,6 +39,97 @@ public:
                char letter =  boxGrid[i][j];
                ans[j][m-i-1] = letter;
             }
+        }
+        return ans;
+        
+    }
+};
+
+
+//app 2 two pointer
+
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int m = boxGrid.size();
+        int n = boxGrid[0].size();
+
+        vector<vector<char>>ans(n,vector<char>(m));
+
+        for(int i = 0; i<m;i++){
+
+            int point_1 = n-1;
+            int point_2 = n-1;
+
+            while(point_2 >= 0){
+                if(boxGrid[i][point_2] == '*'){
+                    point_2--;
+                    point_1 = point_2;
+                    continue;
+                }
+                if(boxGrid[i][point_2] == '#'){
+                    boxGrid[i][point_2] = '.';
+                    boxGrid[i][point_1] = '#';
+                    point_2--;
+                    point_1--;
+                }
+                else{
+                    point_2--;
+                }
+            }
+        }
+
+        for(int i = 0; i<m;i++){
+            for(int j = 0; j<n;j++){
+               char letter =  boxGrid[i][j];
+               ans[j][m-i-1] = letter;
+            }
+        }
+        return ans;
+        
+    }
+};
+
+//app 3 transpose+reverse
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int m = boxGrid.size();
+        int n = boxGrid[0].size();
+
+        vector<vector<char>>ans(n,vector<char>(m));
+
+        for(int i = 0; i<m;i++){
+
+            int point_1 = n-1;
+            int point_2 = n-1;
+
+            while(point_2 >= 0){
+                if(boxGrid[i][point_2] == '*'){
+                    point_2--;
+                    point_1 = point_2;
+                    continue;
+                }
+                if(boxGrid[i][point_2] == '#'){
+                    boxGrid[i][point_2] = '.';
+                    boxGrid[i][point_1] = '#';
+                    point_2--;
+                    point_1--;
+                }
+                else{
+                    point_2--;
+                }
+            }
+        }
+
+        for(int i = 0; i<m;i++){
+            for(int j = 0; j<n;j++){
+               ans[j][i] = boxGrid[i][j];
+            }
+        }
+
+        for(int i = 0; i<n;i++){
+            reverse(ans[i].begin(), ans[i].end());
         }
         return ans;
         
