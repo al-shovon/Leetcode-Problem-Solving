fbc44dfba4bee8697ead86d987db98086d9f9776
al-shovon <shovondab1@gmail.com>
2026-05-13 19:55:57 +0600

    Implement getCommon method for minimum common value

---
+++ Easy/Two Pointer/2540. Minimum Common Value.py
@@ -0,0 +1,16 @@
+#2540. Minimum Common Value ->python
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+
+        n1 = len(nums1)
+        n2 = len(nums2)
+
+        i = j = 0
+
+        while(i<n1 and j <n2):
+            if(nums1[i] == nums2[j]) : return nums1[i]
+
+            if(nums1[i] < nums2[j]) : i+=1
+            else : j+=1
+        return -1
+        
