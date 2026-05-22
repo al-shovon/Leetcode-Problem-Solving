66a3882ca965cf2bf48527042a0ba17d38674fae
al-shovon <shovondab1@gmail.com>
2026-05-23 01:27:11 +0600

    Create 1137. N-th Tribonacci Number.cpp

---
+++ Easy/Dynamic Programming/1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,19 @@
+// 1137. N-th Tribonacci Number
+class Solution {
+public:
+    int dp[39];
+
+    Solution(){
+    memset(dp,-1,sizeof(dp));
+    }
+
+    int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 1;
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+        
+    }
+};
