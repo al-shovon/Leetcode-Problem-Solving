f25dcc6505b615903d159cf1e927adfd1fc7dc51
al-shovon <shovondab1@gmail.com>
2026-06-18 19:56:08 +0600

    Create 1344. Angle Between Hands of a Clock.cpp

---
+++ Medium/Math/1344. Angle Between Hands of a Clock.cpp
@@ -0,0 +1,17 @@
+// 1344. Angle Between Hands of a Clock
+
+
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        
+        double h = ((hour%12)*30) + (minutes * 0.5);
+        double m = (minutes*6);
+        
+        double ans = abs(h-m);
+        
+
+        return min(ans,360-ans);
+        
+    }
+};
