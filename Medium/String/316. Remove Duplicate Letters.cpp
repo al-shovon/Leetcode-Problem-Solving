20f415c2592988a3d90e49d73faed83fbd67d649
Abdullah Al Shovon <shovondab1@gmail.com>
2026-07-20 03:01:54 +0600

    Implement solution for removing duplicate letters

---
+++ Medium/String/316. Remove Duplicate Letters.cpp
@@ -0,0 +1,58 @@
+// 316. Remove Duplicate Letters
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n = s.size();
+        vector<int>freq(26,0);
+        for(auto i : s){
+            freq[i-'a']++;
+        }
+        stack<char>st;
+
+        vector<bool>check(26,0);
+
+        for(int i = 0; i<n;i++){
+            char ch = s[i];
+            freq[ch-'a']--;
+
+            if(st.empty()){
+                check[ch-'a'] = 1;
+                st.push(ch);
+                continue;
+
+            }
+            if(check[ch-'a']) continue;
+
+            
+
+
+            if(ch > st.top()){
+                check[ch-'a'] = 1;
+                st.push(ch);
+                continue;
+            }
+            
+            
+
+            while(!st.empty() && st.top() > ch){
+                if(freq[st.top() - 'a'] != 0){
+                    check[st.top() - 'a'] = 0;
+                    st.pop();
+                }
+                else break;
+            }
+            st.push(ch);
+            check[ch-'a'] = 1;
+            
+
+        }
+        string ans = "";
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
