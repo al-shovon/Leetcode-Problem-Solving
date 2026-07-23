a1dcd01346032875285a751be301a8227848bb51
Abdullah Al Shovon <shovondab1@gmail.com>
2026-07-24 02:43:04 +0600

    Add uniqueXorTriplets method in Solution class
    Implement uniqueXorTriplets method to count unique XOR triplets.

---
+++ Medium/Bit manipulation/3513. Number of Unique XOR Triplets I.py
@@ -0,0 +1,10 @@
+# class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        n = len(nums)
+        if(n<3): return n
+        i = 2
+        while(i<=n): 
+            i<<=1
+        
+        return i
+    
