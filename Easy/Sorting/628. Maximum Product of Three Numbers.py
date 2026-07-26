2862731d28a9287725eb4c70a704127916506363
Abdullah Al Shovon <shovondab1@gmail.com>
2026-07-26 19:38:46 +0600

    Add solution for maximum product of three numbers

---
+++ Easy/Sorting/628. Maximum Product of Three Numbers.py
@@ -0,0 +1,8 @@
+# 628. Maximum Product of Three Numbers
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+
+        return max((nums[0]*nums[1]*nums[-1]), (nums[-1]*nums[-2]*nums[-3]))
+
+        
